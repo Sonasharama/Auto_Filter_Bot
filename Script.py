4257434850a0a0dc50f300fb0cd941318e326cd2
Sonasharama <sonasharmasona785@gmail.com>
2026-09-25 20:11:31 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -269,7 +269,7 @@ Nᴀᴍᴇ : {}
 
 Mᴇꜱꜱᴀɢᴇ : <b>{}</b>"""
     
-    CAPTION = """<b><a href="https://t.me/dreamxbotz">{file_name}</a></b>\n\n<b>⚜️ Powered By : <a href="https://t.me/dreamxbotz">[ ᴅʀᴇᴀᴍxʙᴏᴛᴢ ]</a></b>"""
+    CAPTION = """<b><a href="https://t.me/southottupdate">{file_name}</a></b>\n\n<b>⚜️ Powered By : <a href="https://t.me/southottupdate">[ SouthOttupdate ]</a></b>"""
 
     
     MOVIE_UPDATE_NOTIFY_TXT = """
